c6ad820768465fe48ab4f6b8729b38c60d7a0568
china <china@126.com>
2019-11-12 17:05:22 +0800

    123

---
+++ src/main/java/com/test/Test01.java
@@ -0,0 +1,15 @@
+package com.test;
+
+/**
+ * ClassName:Test01
+ * package:com.test
+ * Description:TODO
+ *
+ * @date:2019/11/12 16:59
+ * @author:yuanpeng
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
